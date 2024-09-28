64542dc31faf1de53b98f826d8b563f9e4eac180
ShahadAlnaamani <149310800+ShahadAlnaamani@users.noreply.github.com>
2024-09-28 11:49:29 +0400

    Add files via upload

---
+++ ToDoListOOP/ToDoListOOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToDoListOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
